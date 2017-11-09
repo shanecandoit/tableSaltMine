f86d16cf4026ad9dfbeca0b73815c7d9ed5a10b0
Shane Page <shanepage@ku.edu>
2017-11-09 11:59:27 -0600

    add dist jar and bat file

---
+++ dist/tableSaltMine.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem command prompt with print output
+rem java -jar "tableSaltMine.jar" 
+
+rem hide command prompt, shows java.exe command prompt
+rem start java -jar "tableSaltMine.jar" 
+
+rem hide command prompts
+start javaw -jar "tableSaltMine.jar" 
