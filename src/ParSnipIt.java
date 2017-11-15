0abff57e81254c75c7ce97ed8e6c65013179c1b3
Shane Page <shanepage@ku.edu>
2017-11-15 16:10:00 -0600

    add code to eval one step at a time. doesnt work?

--- src/ParSnipIt.java
+++ src/ParSnipIt.java
@@ -16,6 +16,10 @@ public class ParSnipIt {
         System.out.println("line = " + line);
         String pre = infixToPrefixConvert(line, false);
         System.out.println("pre = " + pre);
+		
+		
+        String evalOnce = evalSingle(line, false);
+        System.out.println("evalOnce = " + evalOnce);
         
         String tree=prefixToTree(pre);
         System.out.println("tree = " + tree);
@@ -75,7 +79,7 @@ public class ParSnipIt {
         StringTokenizer tokenizer = new StringTokenizer(infix);
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
-            System.out.println("token = " + token);
+            //System.out.println("token = " + token);
             if (isOperand(token)) {
                 operandStack.push(token);
             } else if (token.equals("(") || operatorStack.isEmpty()
@@ -94,12 +98,58 @@ public class ParSnipIt {
             }
         }
         while (!operatorStack.isEmpty()) {
-            System.out.println("operandStack.size() = " + operandStack.size());
+            //System.out.println("operandStack.size() = " + operandStack.size());
             operandStack.push(operationCombine(operatorStack, operandStack, reduce));
-            System.out.println("operandStack.peek() = " + operandStack.peek());
+            //System.out.println("operandStack.peek() = " + operandStack.peek());
         }
         return (operandStack.peek());
-        
+    }
+	
+	public static String evalSingle(String infix, boolean reduce) {
+        Stack<String> operandStack = new Stack<String>();
+        Stack<String> operatorStack = new Stack<String>();
+
+        StringTokenizer tokenizer = new StringTokenizer(infix);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            //System.out.println("token = " + token);
+            if (isOperand(token)) {
+                operandStack.push(token);
+            } else if (token.equals("(") || operatorStack.isEmpty()
+                    || rank(token) > rank(operatorStack.peek())) {
+                operatorStack.push(token);
+            } else if (token.equals(")")) {
+                while (!operatorStack.peek().equals("(")) {
+                    operandStack.push(operationCombine(operatorStack, operandStack, reduce));
+                }
+                operatorStack.pop();
+            } else if (rank(token) <= rank(operatorStack.peek())) {
+                while (!operatorStack.isEmpty() && rank(token) <= rank(operatorStack.peek())) {
+                    operandStack.push(operationCombine(operatorStack, operandStack, reduce));
+                }
+                operatorStack.push(token);
+            }
+        }
+//		we dont want to reduce the entire expression just the 'last bit'
+//        while (!operatorStack.isEmpty()) {
+//            //System.out.println("operandStack.size() = " + operandStack.size());
+//            operandStack.push(operationCombine(operatorStack, operandStack, reduce));
+//            //System.out.println("operandStack.peek() = " + operandStack.peek());
+//        }
+		System.out.println("operandStack.elements().toString() = "  + a2s( operandStack.toArray( ) ));
+		System.out.println("operatorStack.elements().toString() = " + a2s( operatorStack.toArray() ));
+		/*
+		operandStack.elements().toString() = [(+ 1 (* 2 A)),sin(6),]
+		operatorStack.elements().toString() = [+,]
+		
+		i think this means that we can just pop the last from operandStack
+		eval it, append it to operandStack
+		return it
+		*/
+		String lastExprEvald = evalSingle(operandStack.pop(), true);
+		String restExprNotEvald = a2sNoArrayNotation( operandStack.toArray() );
+		String result = restExprNotEvald + lastExprEvald;
+        return result;
     }
 
     public static String prefixToTree(String pre) {
@@ -107,7 +157,7 @@ public class ParSnipIt {
         System.out.println("pre.length() = " + pre.length());
         //pre = (+ (+ 1 (* 2 3)) sin(6))
         
-        
+        String indent="-";
         String out = "\n";
         int deep=0;
         for(char ch:pre.toCharArray()){
@@ -124,7 +174,7 @@ public class ParSnipIt {
             else if(ch==' '){
                 out+="\n";
                 for(int i=0;i<deep;i++){
-                    out+="_";
+                    out+=indent;
                 }
             }
             else{
@@ -144,5 +194,22 @@ tree =
 ---A))
 -sin(6))
     */
+
+	private static String a2s(Object[] toArray) {
+		String out="[";
+		for(Object o:toArray){
+			out+=o+",";
+		}
+		out+="]";
+		return out;
+	}
+	private static String a2sNoArrayNotation(Object[] toArray) {
+		String out="";
+		for(Object o:toArray){
+			out+=o+" ";
+		}
+		//out+="";
+		return out;
+	}
 
 }
